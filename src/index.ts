8d698a0167d3a5da9c9d45c28f3847b8090e7ede
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-02-25 14:07:25 +0800

    text component

--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,113 @@
 type childrenElementConfig = {
-    type: string;
-    props: vDomProps;
-    children?: childrenElementConfig[];
+  type: string;
+  props: vDomProps;
+  children?: childrenElementConfig[];
 };
 
 type vDomProps = {
-    key: string;
-    [prop: string]: string;
+  key: string;
+  [prop: string]: string;
 };
 
-
 class VDom {
-    type: string|Function;
+  type: string | Function;
 
-    props: vDomProps;
-    children: childrenElementConfig[];
+  props: vDomProps;
+  children: childrenElementConfig[];
 
-    constructor(
-        type: string,
-        props: vDomProps,
-        children: childrenElementConfig[]
-    ) {
-        this.type = type;
-        this.props = props;
-        this.children = children;
-    }
-}
-
-function createElement(
+  constructor(
     type: string,
     props: vDomProps,
     children: childrenElementConfig[]
+  ) {
+    this.type = type;
+    this.props = props;
+    this.children = children;
+  }
+}
+
+function createElement(
+  type: string,
+  props: vDomProps,
+  children: childrenElementConfig[]
 ) {
-    return new VDom(type, props, children);
+  return new VDom(type, props, children);
 }
 
 class ReactComponent {
-    #vDom: VDom
+  mountComponent(): string {
+    throw "you must override this method!";
+  }
 
-    constructor(element: VDom) {
-        this.#vDom = element;
-    }
-
-    mountComponent():string { 
-        throw "you must override this method!"
-    }
-
-    updateComponent() { 
-        throw "you must override this method!"
-    }
+  updateComponent() {
+    throw "you must override this method!";
+  }
 }
 
 class ReactDomComponent extends ReactComponent {
-    constructor(element: VDom) {
-        super(element)
-    }
+  vDom: VDom;
+  constructor(element: VDom) {
+    super();
+    this.vDom = element;
+  }
 
-    mountComponent() { return ""}
+  mountComponent() {
+    return "";
+  }
 
-    updateComponent() { }
+  updateComponent() {}
 }
 
 class ReactCompositeComponent extends ReactComponent {
-    constructor(element: VDom) {
-        super(element)
-    }
-    
-    mountComponent() {return "" }
+  vDom: VDom;
+  constructor(element: VDom) {
+    super();
+    this.vDom = element;
+  }
 
-    updateComponent() { }
+  mountComponent() {
+    return "";
+  }
+
+  updateComponent() {}
 }
 
 class ReactTextComponent extends ReactComponent {
-    constructor(element:string|number|bigint|boolean) {
-        super({} as any )
-    }
-    
-    mountComponent() {return "" }
+  vDom: string | number | bigint | boolean;
 
-    updateComponent() { }
+  constructor(element: string | number | bigint | boolean) {
+    super();
+    this.vDom = element;
+  }
+
+  mountComponent() {
+    return `<span>${this.vDom}</span>`;
+  }
+
+  updateComponent() {}
 }
 
-function instantiateReactComponent(element: VDom|string|number|bigint|boolean) {
-
-    if (typeof element !=="object") {
-        return new ReactTextComponent(element)
-    }else if(typeof element.type ==="string"){
-        return new ReactDomComponent(element)
-    }else{
-        return new ReactCompositeComponent(element);
-    }
-
+function instantiateReactComponent(
+  element: VDom | string | number | bigint | boolean
+) {
+  if (typeof element !== "object") {
+    return new ReactTextComponent(element);
+  } else if (typeof element.type === "string") {
+    return new ReactDomComponent(element);
+  } else {
+    return new ReactCompositeComponent(element);
+  }
 }
 
-
-const React={
-    ReactComponent,
-    createElement,
-    render(element: VDom|string|number|bigint|boolean,container:HTMLElement){
-        const componentInstance = instantiateReactComponent(element);
-        var markup = componentInstance.mountComponent()
-        container.innerHTML=markup;
-    }
-}
+const React = {
+  ReactComponent,
+  createElement,
+  render(
+    element: VDom | string | number | bigint | boolean,
+    container: HTMLElement
+  ) {
+    const componentInstance = instantiateReactComponent(element);
+    var markup = componentInstance.mountComponent();
+    container.innerHTML = markup;
+  }
+};
